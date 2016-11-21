3a244058f9bbbd609bdcace3828c3757bb5d9aa5
dj.jellings <dj.jellings@collegeamerica.edu>
2016-11-21 13:43:31 -0700

    Lunch 11-21-2016

--- Engine/Game.cpp
+++ Engine/Game.cpp
@@ -43,4 +43,15 @@ void Game::UpdateModel()
 void Game::ComposeFrame()
 {
 	gfx.PutPixel(700, 500, /*RGB --->*/ 255, 255, 255);
+	gfx.PutPixel(700, 500, /*RGB --->*/ 255, 255, 255);
+	gfx.PutPixel(700, 500, /*RGB --->*/ 255, 255, 255);
+	gfx.PutPixel(700, 500, /*RGB --->*/ 255, 255, 255);
+	gfx.PutPixel(700, 500, /*RGB --->*/ 255, 255, 255);
+	gfx.PutPixel(700, 500, /*RGB --->*/ 255, 255, 255);
+	gfx.PutPixel(700, 500, /*RGB --->*/ 255, 255, 255);
+	gfx.PutPixel(700, 500, /*RGB --->*/ 255, 255, 255);
+	gfx.PutPixel(700, 500, /*RGB --->*/ 255, 255, 255);
+	gfx.PutPixel(700, 500, /*RGB --->*/ 255, 255, 255);
+	gfx.PutPixel(700, 500, /*RGB --->*/ 255, 255, 255);
+	gfx.PutPixel(700, 500, /*RGB --->*/ 255, 255, 255);
 }
